25ca3be1938ad507dc61320d798805379a586af3
Mateusz <mateusz.pusiewicz@ing.com>
2022-12-02 09:54:41 +0100

    Initial Commit

---
+++ Twitter Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Twitter Clone
+//
+//  Created by Pusiewicz, M. (Mateusz) on 02/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
